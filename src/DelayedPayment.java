bf4c263eebe3a07f3b4eb8111f3c58cf0bd44484
itayrazum <itayrazum@gmail.com>
2022-11-09 14:44:44 +0200

    astetika :)

---
+++ src/DelayedPayment.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class DelayedPayment extends Payment{
+    private Date paymentDate;
+
+    public DelayedPayment(String ID, Date PAID, Float TOTAL, String DETAILS, Date PAYMENTDATE) {
+        super(ID, PAID, TOTAL, DETAILS);
+        this.paymentDate = PAYMENTDATE;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
---
+++ src/Payment.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class Payment {
+    private String id;
+    private Date paid;
+    private Float total;
+    private String details;
+
+    public Payment(String ID, Date PAID, Float TOTAL, String DETAILS){
+        this.id = ID;
+        this.paid = PAID;
+        this.total = TOTAL;
+        this.details = DETAILS;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Date paid) {
+        this.paid = paid;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}

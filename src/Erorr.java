47f7e03c0908b72f40af954693717f2b839dccb3
yuvalweiz <106019626+yuvalweiz@users.noreply.github.com>
2022-11-14 14:26:26 +0200

    replaced arrays with vectors, and added the relevant methods.

--- src/Erorr.java
+++ src/Erorr.java
@@ -1,2 +1,44 @@
 public abstract class Erorr extends Exception{
-}
+
+    public Erorr(String s) {
+    }
+
+    public class id_is_wrong_EX extends Erorr
+    {
+        public id_is_wrong_EX() {
+            super("The ID you entered is wrong.");
+        }
+    }
+    public class pass_is_wrong_EX extends Erorr
+    {
+
+        public pass_is_wrong_EX() {
+            super("The ID you entered is wrong.");
+        }
+    }
+    public class there_is_user_connected_EX extends Erorr
+    {
+        public there_is_user_connected_EX() {
+            super("The ID you entered is wrong.");
+        }
+    }
+    public class there_is_no_user_connected_EX extends Erorr
+    {
+        public there_is_no_user_connected_EX() {
+            super("The ID you entered is wrong.");
+        }
+    }
+    public class this_user_is_not_connected_EX extends Erorr
+    {
+        public this_user_is_not_connected_EX() {
+            super("The ID you entered is wrong.");
+        }
+    }
+
+
+    }
+
+
+
+
+// id_is_wrong, pass_is_wrong,there_is_user_connected,succses,there_is_no_user_connected,this_user_is_not_connected;
--- src/PremiumAccount.java
+++ src/PremiumAccount.java
@@ -4,6 +4,6 @@ public class PremiumAccount extends Account {
 
 
     public PremiumAccount(String _id, Customer _customer) {
-        super(_customer);
+        super( _customer);
     }
 }

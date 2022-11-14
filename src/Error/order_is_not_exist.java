674e6d03795734fadca82f7eeed7c0eefb3d5800
itayrazum <itayrazum@gmail.com>
2022-11-14 15:59:24 +0200

    more expestions to error

---
+++ src/Error/order_is_not_exist.java
@@ -0,0 +1,7 @@
+package Error;
+
+public class order_is_not_exist extends Erorr {
+    public order_is_not_exist() {
+        super("The order is not exist.");
+    }
+}
---
+++ src/Error/product_is_not_exist.java
@@ -0,0 +1,8 @@
+package Error;
+
+public class product_is_not_exist extends Erorr {
+
+    public product_is_not_exist() {
+        super("The product is not exists.");
+    }
+}
